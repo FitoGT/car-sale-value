b8ed5a11753459160c8b8afc36430665c8f7b222
Adolfo <adolfo.guevara.11@gmail.com>
2023-10-18 15:39:54 -0300

    reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorators';
+import { User } from '../users/user.entity';
+import { ReportDto } from './dto/report.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportService: ReportsService) { }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportService.create(body, user)
+  }
+
+}
