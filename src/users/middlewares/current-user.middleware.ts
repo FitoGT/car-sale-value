ba787199620c6ceb6709f91ec7b03f054f8efa87
Adolfo <adolfo.guevara.11@gmail.com>
2023-10-18 17:18:06 -0300

    fixing type deinition

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,6 +1,15 @@
 import { NestMiddleware, Injectable } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 import { UsersService } from "../users.service";
+import { User } from "../user.entity";
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -10,7 +19,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
 
     if (userId) {
       const user = await this.userService.findOne(userId)
-      // @ts-ignore
+
       req.currentUser = user
     }
 
